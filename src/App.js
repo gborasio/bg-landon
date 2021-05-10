b21caecb73f89b1822957a96eb86b396c46b5a05
gborasio <gborasio@gmail.com>
2021-05-11 00:45:31 +0200

    json files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React from 'react';
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import Main from './components/Main.js'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header/>
+    
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React is perfect
-        </a>
-      </header>
-    </div>
+      <Main/>
+     <Footer/>
+      </div>
   );
+}
 }
 
 export default App;
